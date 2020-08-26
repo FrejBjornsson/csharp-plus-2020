07c3ed9075ae5d5bf1aaa49f8afc7d5e55a1534e
DESKTOP-U8MF4KS\frej_ <frej.bjornsson@protonmail.com>
2020-08-26 11:54:06 +0200

    yo

---
+++ Session02/session02ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace session02ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
