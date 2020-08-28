b326de280cee585f7706e77cecb9f63902e3de9c
DESKTOP-U8MF4KS\frej_ <frej.bjornsson@protonmail.com>
2020-08-28 09:33:28 +0200

    test

---
+++ Session02Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Session02Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in tre värden, separerat med kommatecken");
+            var input = Console.ReadLine();
+            var inputArray = input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in inputArray)
+            {
+                Console.WriteLine("Värdet är" + number);
+            }
+        }
+    }
+}
